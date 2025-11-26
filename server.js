8a26973d44ea6dcf5bd32d7103d47e84ced9db9b
daliya0606 <Sultanova.daliya@bk.ru>
2025-11-26 09:56:21 +0300

    qwe

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const {
   YMPaymentFormBuilder,
   YMNotificationChecker,
   YMNotificationError
-} = require('yoomoney-sdk');
+} = require('./lib/yoomoney-sdk');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
